5309cbc06413bc15b8afaec2e38c0a5b782cd856
stefanpreda <stefan.alexandru.preda@gmail.com>
2016-11-25 15:58:48 +0200

    Added input field, scaled and positioned according to viewport.

---
+++ WordRacer/Assets/Scripts/ViewportAdjust.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewportAdjust : MonoBehaviour {
+
+    public Camera camera = null;
+    public float viewport_positionX = 0.3f;
+    public float viewport_positionY = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+        RectTransform targetTransform = gameObject.GetComponent<RectTransform>();
+        targetTransform.position = camera.ViewportToScreenPoint(new Vector3(viewport_positionX, viewport_positionY, 0.0f));
+    }
+
+}
---
+++ WordRacer/Assets/Scripts/WordBuilder.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WordBuilder : MonoBehaviour {
+
+    List<GameObject> currentWord;
+
+    public float start_positionX = 0.5f;
+    public float start_positionY = 0.4f;
+
+	void Start () {
+        currentWord = new List<GameObject>();
+        ChangeWord();   
+	}
+
+    void Update()
+    {
+        //ChangeWord();
+    }
+
+    void ChangeWord()
+    {
+        foreach (GameObject o in currentWord)
+            Destroy(o);
+        currentWord.Clear();
+
+        string new_word = getWord();
+        float spacing = 0;
+
+        foreach (char c in new_word)
+        {
+            GameObject obj = new GameObject();
+            obj.AddComponent<GUIText>();
+            obj.GetComponent<GUIText>().text = c.ToString();
+            obj = Instantiate(obj, new Vector3(start_positionX + spacing, start_positionY, 0), Quaternion.identity, transform) as GameObject;
+            spacing += 0.01f;
+            currentWord.Add(obj);
+        }
+    }
+	
+    string getWord()
+    {
+        return "Dynamic Current Phrase";
+    }
+}
