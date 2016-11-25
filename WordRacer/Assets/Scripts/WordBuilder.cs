32e5580dba4e6a897158687ca77ccaace8c877ed
stefanpreda <stefan.alexandru.preda@gmail.com>
2016-11-25 19:41:39 +0200

    Added word building, each character a different gameobject. Positioning according to viewport.

--- WordRacer/Assets/Scripts/WordBuilder.cs
+++ WordRacer/Assets/Scripts/WordBuilder.cs
@@ -8,6 +8,9 @@ public class WordBuilder : MonoBehaviour {
 
     public float start_positionX = 0.5f;
     public float start_positionY = 0.4f;
+    public float word_spacing = 0.005f;
+
+    public Camera camera = null;
 
 	void Start () {
         currentWord = new List<GameObject>();
@@ -34,7 +37,16 @@ public class WordBuilder : MonoBehaviour {
             obj.AddComponent<GUIText>();
             obj.GetComponent<GUIText>().text = c.ToString();
             obj = Instantiate(obj, new Vector3(start_positionX + spacing, start_positionY, 0), Quaternion.identity, transform) as GameObject;
-            spacing += 0.01f;
+
+            Rect r = obj.GetComponent<GUIText>().GetScreenRect();
+            Vector3 posX = camera.ScreenToViewportPoint(new Vector3(r.x, r.y, 0f));
+            Vector3 max_posX = camera.ScreenToViewportPoint(new Vector3(r.xMax, r.y, 0f));
+
+            //Adding character lengths
+            spacing += max_posX.x - posX.x;
+
+            //Adding extra space between characters
+            spacing += word_spacing;
             currentWord.Add(obj);
         }
     }
