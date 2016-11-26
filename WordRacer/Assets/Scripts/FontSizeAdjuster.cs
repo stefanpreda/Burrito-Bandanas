fe30b6491a31e6441c850e8ae8e88cbbab353554
Napoleon1993 <me_coco2007@yahoo.com>
2016-11-26 14:21:48 +0200

    Added score display. Adjusted size to screen size.

---
+++ WordRacer/Assets/Scripts/FontSizeAdjuster.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FontSizeAdjuster : MonoBehaviour {
+
+    public int text_size_divisor = 25;
+
+    void Start () {
+        gameObject.GetComponent<GUIText>().fontSize = Screen.width / text_size_divisor;
+    }
+}
