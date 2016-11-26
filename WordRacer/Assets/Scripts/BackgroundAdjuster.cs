6a3a5998c9980ae94d2d78282b3404345f90ae67
Napoleon1993 <me_coco2007@yahoo.com>
2016-11-26 15:26:12 +0200

    Added a background. Fit background to screen size.

---
+++ WordRacer/Assets/Scripts/BackgroundAdjuster.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundAdjuster : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ResizeSpriteToScreen();
+	}
+
+    void ResizeSpriteToScreen()
+    {
+        var sr = gameObject.GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        var width = sr.sprite.bounds.size.x;
+        var height = sr.sprite.bounds.size.y;
+
+        var worldScreenHeight = Camera.main.orthographicSize * 2.0;
+        var worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        transform.localScale = new Vector3((float)worldScreenWidth / width, (float)worldScreenHeight / height, transform.localScale.z);
+    }
+}
